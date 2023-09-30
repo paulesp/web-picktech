c19663287e7cdee6cabb05aea3daeb11a04e255d
paulesp <118700247+paulesp@users.noreply.github.com>
2023-09-30 18:26:19 -0500

    Update db_picktech.php

--- db_picktech.php
+++ db_picktech.php
@@ -1,6 +1,9 @@
 <?php 
-    $host = "localhost";
-    $user = "id21332320_melmoth";
-    $pass = "p98kkmshjJn]UO";
-    $db = "id21332320_bdproyecto";
+session_start():
+$DB_HOST=$_ENV["DB_HOST"];
+$DB_USER=$_ENV["DB_USER"];
+$DB_PASSWORD=$_ENV["DB_PASSWORD"];
+$DB_NAME=$_ENV["DB_NAME"];
+$DB_PORT=$_ENV["DB_PORT"];
+$db=mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
 ?>
