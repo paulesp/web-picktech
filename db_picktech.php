e5da148fea9d66f7705c3400a33a38a3232c1b24
paulesp <118700247+paulesp@users.noreply.github.com>
2023-10-01 01:20:42 -0500

    Update db_picktech.php

--- db_picktech.php
+++ db_picktech.php
@@ -1,7 +1,5 @@
 <?php
-    ob_start();
-?>
-<?php
+
 session_start();
 $DB_HOST=$_ENV["DB_HOST"];
 $DB_USER=$_ENV["DB_USER"];
@@ -9,5 +7,5 @@ $DB_PASSWORD=$_ENV["DB_PASSWORD"];
 $DB_NAME=$_ENV["DB_NAME"];
 $DB_PORT=$_ENV["DB_PORT"];
 $db=mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
-ob_flush();
+
 ?>
